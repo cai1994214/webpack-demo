6206cb7a81dc43f5b5584fe290e3bec18ca94fd2
renjie_cai <renjie.cai@ihoment.com>
2022-04-07 18:37:51 +0800

    add postcss-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import Header from "./header.js";
-import Sidebar from "./sidebar.js";
+
 import demo from './demo1.png';
+import CreateAvator from './createAvator.js'
+import style from './index.scss';
+
+
+CreateAvator(demo);
 
 var dom = document.getElementById('root');
 var img = new Image();
+img.setAttribute('class', style.avator);// 使用css模块化
 img.src = demo;
-dom.appendChild(img);
-new Header();
-new Sidebar();
+dom.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,57 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HemlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    mode: 'production', //默认是production
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'boundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                use: {
-                    loader: 'file-loader',
-                }
-            }
-        ]
-    }
-}
+	entry: {
+		main: "./src/index.js",
+	},
+	mode: "production", //默认是production
+	output: {
+		publicPath: "./",
+		path: path.resolve(__dirname, "dist"),
+		filename: "boundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						//placeholder: '占位符'
+						name: "[name]_[hash].[ext]",
+						outputPath: "images/", //打包后的图片存放的文件夹
+						limit: 10240, //10k
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader", "postcss-loader"], //cssloader 处理css相关文件成一个css 然后通过style-loader 将css插入到html中
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+                            //在 css-loader 前应用的 loader 的数量 比如在scss文件里重新引入别的scss文件 需要配置两个loader重新编译
+							importLoaders: 2,
+                            modules: true, //使用css模块化
+						},
+					},
+					"sass-loader",
+					"postcss-loader",
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HemlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+	],
+};
